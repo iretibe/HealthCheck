a463a01d75785b22d837def7b6258f2f747628b6
Somad <iretibe@users.noreply.github.com>
2021-03-09 17:55:33 +0000

    Clean Angular solution by adding approutingmodule

---
+++ ClientApp/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+//import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from './app.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { HomeComponent } from './home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavMenuComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
+    HttpClientModule,
+    FormsModule,
+    AppRoutingModule
+    //RouterModule.forRoot([
+    //  { path: '', component: HomeComponent, pathMatch: 'full' },
+    //  //{ path: 'counter', component: CounterComponent },
+    //  //{ path: 'fetch-data', component: FetchDataComponent },
+    //])
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
